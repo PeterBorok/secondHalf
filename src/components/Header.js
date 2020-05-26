32f85e9e7bab2704c3c962b7539d2620af938c63
PeterBorok <peter.borok@gmail.com>
2020-05-26 08:28:47 +0200

    create Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header>
+            <img
+                style={{ width: "35%" }}
+                src="https://cdn.clipart.email/53cf60a91161de45b077701e67d9ba85_premier-league-logo-png-and-vector-logo-download_2160-830.png"
+                alt="Page header"
+            />
+        </header>
+    );
+}
