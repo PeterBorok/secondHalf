b7e6d026b67c57710b9cf784b92c1d02a66fa0c8
PeterBorok <peter.borok@gmail.com>
2020-05-26 13:59:26 +0200

    add video background to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import video from "../video/uefa.mp4";
 
 export default function Header() {
     return (
@@ -8,6 +9,22 @@ export default function Header() {
                 src="https://cdn.clipart.email/53cf60a91161de45b077701e67d9ba85_premier-league-logo-png-and-vector-logo-download_2160-830.png"
                 alt="Page header"
             />
+            <video id="videoBg"
+                autoPlay
+                muted
+                loop
+                style={videoStyle}
+            >
+                <source src={video} type="video/mp4" />
+            </video>
         </header>
     );
+}
+
+const videoStyle = {
+    position: "fixed",
+    width: "100%",
+    left: 0,
+    top: 0,
+    zIndex: -1,
 }
