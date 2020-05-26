9754ec878a6dab9f7fedb137a9c9f8701726d8d1
PeterBorok <peter.borok@gmail.com>
2020-05-26 14:03:40 +0200

    add header and navbar to main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import video from "./video/uefa.mp4";
 import "./App.css";
 
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"><Header /></div>;
+  return <div className="App">
+    <Header />
+    <Navbar />
+
+  </div>;
 }
 
 export default App;
+
