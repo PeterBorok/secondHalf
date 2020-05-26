e55ddf1ccff6d2b00f5eefc4e0d2029186ee54e2
Kornél <leitner.kornel@gmail.com>
2020-05-26 21:06:20 +0200

    Route added for the Selection Page (League/Teams)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Leagues from "./components/Leagues";
+import SelectionPage from "./components/pages/SelectionPage";
 
 import "./App.css";
 
@@ -10,13 +11,23 @@ function App() {
       <Router>
         <div className="App">
           {/*Selection page route will be here*/}
-          <Route path='/leagues'
-          render={()=> (<React.Fragment>
-            <Leagues/>
-          </React.Fragment>)}
+          <Route
+              exact
+              path='/leagues'
+              render={() => (
+                  <React.Fragment>
+                    <Leagues/>
+                  </React.Fragment>)}
+          />
+          <Route
+              path={'/league/:leagueId'}
+              render={() => (
+                  <React.Fragment>
+                    <SelectionPage/>
+                  </React.Fragment>
+              )}
           />
         </div>
-        ;
       </Router>
   );
 }
