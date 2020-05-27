805855a48038cb68a4b3b223b59eafe0363d17b4
Kornél <leitner.kornel@gmail.com>
2020-05-27 15:18:39 +0200

    Routes added for team and league detail pages

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {FavouriteProvider} from "./components/context/FavouriteTeamContext";
 import Leagues from "./components/Leagues";
 import SelectionPage from "./components/pages/SelectionPage";
+import LeagueDetails from "./components/LeagueDetails";
+import FavouriteTeams from "./components/pages/FavouriteTeams";
 
 import "./App.css";
-import FavouriteTeams from "./components/pages/FavouriteTeams";
 
 function App() {
   return (
@@ -23,10 +24,27 @@ function App() {
                     </React.Fragment>)}
             />
             <Route
+                exact
                 path={'/league/:leagueId'}
                 render={() => (
                     <React.Fragment>
                       <SelectionPage/>
+                    </React.Fragment>
+                )}
+            />
+            <Route
+                path={'/league/details/:leagueId'}
+                render={() => (
+                    <React.Fragment>
+                      <LeagueDetails/>
+                    </React.Fragment>
+                )}
+            />
+            <Route
+                path={'/team/:teamId'}
+                render={() => (
+                    <React.Fragment>
+                      <div>Team detail page comes here</div>
                     </React.Fragment>
                 )}
             />
