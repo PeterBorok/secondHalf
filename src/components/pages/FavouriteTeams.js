7dceed4891a9f386de8ad970355358303032e290
Kornél <leitner.kornel@gmail.com>
2020-05-27 15:27:56 +0200

    Fixed renamed class names

--- src/components/pages/FavouriteTeams.js
+++ src/components/pages/FavouriteTeams.js
@@ -1,5 +1,6 @@
 import React, {useState, useContext} from 'react';
 import {FavouriteTeamContext} from "../context/FavouriteTeamContext";
+import TeamCard from "../TeamCard";
 
 const FavouriteTeams = () => {
   const [favouriteTeams, setFavouriteTeams] = useContext(FavouriteTeamContext);
@@ -8,22 +9,9 @@ const FavouriteTeams = () => {
       <div>
         Favourite Teams
         <main>
-          <section className="league-cards">
+          <section className="cards">
             {favouriteTeams.map((entry) => (
-                <div className="league-card" key={entry.id}>
-                  <div className="league-card__image-container">
-                    <img src={entry.badge} alt={entry.name}/>
-                  </div>
-                  <div className="league-card__content">
-                    <p className="league-card__title text--medium">
-                      {entry.name}
-                    </p>
-                    <div className="league-card__info">
-                      <p className="text--medium">{entry.stadiumName}</p>
-                      <p className="league-card__teams text--medium">{entry.formedYear}</p>
-                    </div>
-                  </div>
-                </div>
+                <TeamCard entry={entry} key={entry.id}/>
             ))}
           </section>
         </main>
