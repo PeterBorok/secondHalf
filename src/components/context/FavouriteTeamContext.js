c1be44037dc89b2da53ad2ebb27b7a932a5ac9ab
Kornél <leitner.kornel@gmail.com>
2020-05-28 14:57:37 +0200

    Remove hard coded teams, starting with empty Favourite Context

--- src/components/context/FavouriteTeamContext.js
+++ src/components/context/FavouriteTeamContext.js
@@ -1,24 +1,9 @@
 import React, {useState, createContext} from "react";
 
-export const FavouriteTeamContext = createContext();
+export const FavouriteTeamContext = createContext(undefined, undefined);
 
 export const FavouriteProvider = (props) => {
-  const [favouriteTeams, setFavouriteTeams] = useState([
-    {
-      badge: "https://www.thesportsdb.com/images/media/team/badge/a1af2i1557005128.png",
-      formedYear: "1892",
-      id: "133604",
-      name: "Arsenal",
-      stadiumName: "Emirates Stadium"
-    },
-    {
-      badge: "https://www.thesportsdb.com/images/media/team/badge/yvwvtu1448813215.png",
-      formedYear: "1905",
-      id: "133610",
-      name: "Chelsea",
-      stadiumName: "Stamford Bridge"
-    }
-  ]);
+  const [favouriteTeams, setFavouriteTeams] = useState([]);
 
   return (
       <FavouriteTeamContext.Provider value={[favouriteTeams, setFavouriteTeams]}>
